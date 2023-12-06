473aa66cdbd752dfdcad30556ad055d1b26a1e5d
Nico Vincent <nicolqs@gmail.com>
2023-12-06 18:08:16 -0500

    Add SST Table

--- sst.config.ts
+++ sst.config.ts
@@ -1,16 +1,30 @@
 import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+import { NextjsSite, Table } from "sst/constructs";
 
 export default {
   config(_input) {
     return {
       name: "tinker",
       region: "us-east-1",
+      profile: "VINCENT",
     };
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const table = new Table(stack, "waitlist", {
+        fields: {
+          id: "string",
+          email: "string",
+          createdAt: "number",
+        },
+        primaryIndex: { partitionKey: "email" },
+      });
+
+      const site = new NextjsSite(stack, "site", {
+        bind: [table],
+      });
+
+      site.attachPermissions([table]);
 
       stack.addOutputs({
         SiteUrl: site.url,
